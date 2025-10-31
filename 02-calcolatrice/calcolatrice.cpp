fe177f699359921591d54b843ee42cd1c2d10b7a
laaLinuxTablet <a.laaouatni@studenti.unibg.it>
2025-10-31 22:29:15 +0000

    finito programma 02-calcolatrice ma non compilato o controllato

--- 02-calcolatrice/calcolatrice.cpp
+++ 02-calcolatrice/calcolatrice.cpp
@@ -1,28 +1,12 @@
 #include <iostream>
 
-const int  N_INPUTS                    = 3;
+const int N_INPUTS = 3;
 // 3 array di stringhe di 2 caratteri però c++ vuole [3] perchè oltre ai 2 caratteri esiste anche
 // un carattere nullo '\0'
 const char intKeys[N_INPUTS][N_INPUTS] = {"n1", "n2", "n3"};
-const char validCommands[5][2] = {"A", "B", "C", "D", "X"};
+const char validCommands[5][2]         = {"A", "B", "C", "D", "X"};
 
 namespace laa {
-  int addizione(int n1, int n2, int n3) {
-    return n1 + n2 + n3;
-  };
-  
-  int prodotto(int n1, int n2) {
-    return n1 * n2;
-  };
-  
-  int sottrazione(int n1, int n2) {
-    return n1 - n2;
-  };
-  
-  double divisione(double n1, double n2) {
-    return n1 / n2;
-  };
-  
   void printMenu() {
     std::cout << "istruzioni che puoi usare:\n";
     std::cout << "A: addizione:\tn1+n2+n3\n";
@@ -31,33 +15,58 @@ namespace laa {
     std::cout << "D: divisione:\tn1/n2\n";
     std::cout << "X: uscita programma";
   };
+  
+  int addizione(int n1, int n2, int n3) {
+    return n1 + n2 + n3;
+  };
+
+  int prodotto(int n1, int n2) {
+    return n1 * n2;
+  };
+
+  int sottrazione(int n1, int n2) {
+    return n1 - n2;
+  };
+
+  double divisione(double n1, double n2) {
+    return n1 / n2;
+  };
+
 
   bool isGoodCommand(char thisString[2]) {
-    for(int i=0; i<5; i++) {
+    for (int i = 0; i < 5; i++) {
       const char thisValidCommand[2] = validCommands[i];
-      if(thisValidCommand == thisString) return true;
+      if (thisValidCommand == thisString) { return true; }
     };
     return false;
   }
-}
-
-
+} // namespace laa
 
 int main() {
-  int        intValues[N_INPUTS];
-  char choosedCommand[2];
-
-  for (int i = 0; i < N_INPUTS; i++) {
-    do {
-      std::cout << "inserire " << intKeys[i] << ": ";
-      std::cin >> intValues[i];
-    } while (!(intValues[i] >= 1 && intValues[i] <= 100));
-  };
-
   do {
-    laa::printMenu();
-    std::cin>>choosedCommand;
-  } while(!(laa::isGoodCommand(choosedCommand)));
+    int  intValues[N_INPUTS];
+    char choosedCommand[2];
 
-  return 0;
+    for (int i = 0; i < N_INPUTS; i++) {
+      do {
+        std::cout << "inserire " << intKeys[i] << ": ";
+        std::cin >> intValues[i];
+      } while (!(intValues[i] >= 1 && intValues[i] <= 100));
+    };
+
+    do {
+      laa::printMenu();
+      std::cin >> choosedCommand;
+    } while (!(laa::isGoodCommand(choosedCommand)));
+
+    if (choosedCommand == "X") {
+      std::cout << "grazie per aver utilizzato l'app. arrivederci!";
+      return 0;
+    }
+
+    if (choosedCommand == "A") { laa::addizione(intValues[0], intValues[1], intValues[2]); }
+    if (choosedCommand == "B") { laa::prodotto(intValues[1], intValues[2]); }
+    if (choosedCommand == "C") { laa::sottrazione(intValues[3], intValues[1]); }
+    if (choosedCommand == "D") { laa::divisione(intValues[1], intValues[2]); }
+  } while (true);
 };
