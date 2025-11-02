81f35967d802264da69bb297065de4cedfb73ce3
laaLinuxTablet <a.laaouatni@studenti.unibg.it>
2025-11-02 17:49:41 +0000

    iniziato assegnazione numero 3, per adesso ho scritto solo il menu

---
+++ 03-array/array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+namespace laa {
+  void printMenu() {
+    std::cout << "menu:" << std::endl;
+    std::cout << "➜ 1. media" << std::endl;
+    std::cout << "➜ 2. massimo" << std::endl;
+    std::cout << "➜ 3. minimo" << std::endl;
+    std::cout << "➜ 4. rigenerare array" << std::endl;
+    std::cout << "➜ 5. calcolare la moda" << std::endl;
+    std::cout << "➜ 6. uscire dal programma" << std::endl;
+  }
+}; // namespace laa
+
+int main() {}
